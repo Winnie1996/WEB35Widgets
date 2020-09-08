95f9b0b4d68174567482b05518dec0e146e910f0
HungT <64987549+Winnie1996@users.noreply.github.com>
2020-09-08 09:46:38 -0700

    Finished besides programmers

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -2,7 +2,7 @@
 MOODS Instructions
 
 Watch this short video:
-https://tk-assets.lambdaschool.com/fe491c4f-395b-425c-b62e-3fbebf3cbb3f_moods-clip.gif
+  https://tk-assets.lambdaschool.com/fe491c4f-395b-425c-b62e-3fbebf3cbb3f_moods-clip.gif
 
 How many slices of state do you think are necessary to act as "sources of truth" for all
 the things that change in this widget? Give it some thought before continuing reading!
@@ -28,39 +28,54 @@ STEPS 4, 5, 6:
   Inside these click handlers set the correct mood, using 'setMood' and the variables below the imports.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from "react"; /* STEP 0 */
 
-const initialMood = 'Not sure how I feel';
-const happyMood = 'Quite happy!';
-const sadMood = 'Rather sad';
+const initialMood = "Not sure how I feel";
+const happyMood = "Quite happy!";
+const sadMood = "Rather sad";
 
 export default function Moods() {
   /* STEP 1 */
+  const [mood, setMood] = useState(initialMood);
 
   const makeHappy = () => {
     /* STEP 4 */
+    setMood(happyMood);
   };
   const makeSad = () => {
     /* STEP 5 */
+    setMood(sadMood);
   };
   const reset = () => {
     /* STEP 6 */
+    setMood(initialMood);
   };
 
   const style = {
-    fontSize: '1.5em',
-    marginBottom: '0.3em',
-    color: 'crimson', /* STEP 2 */
+    fontSize: "1.5em",
+    marginBottom: "0.3em",
+    color: mood === happyMood ? "royalblue" : "crimson" /* STEP 2 */,
   };
 
   return (
-    <div className='widget-moods container'>
+    <div className="widget-moods container">
       <h2>Moods</h2>
-      <div id='mood' style={style}>Not sure how I feel</div> {/* STEP 3 */}
+      <div id="mood" style={style}>
+        {/* {Not sure how I feel} */}
+        {mood}
+      </div>
+      {""}
+      {/* STEP 3 */}
       <div>
-        <button id='makeHappy' onClick={makeHappy}>Make Happy</button>
-        <button id='makeSad' onClick={makeSad}>Make Sad</button>
-        <button id='resetMood' onClick={reset}>Reset</button>
+        <button id="makeHappy" onClick={makeHappy}>
+          Make Happy
+        </button>
+        <button id="makeSad" onClick={makeSad}>
+          Make Sad
+        </button>
+        <button id="resetMood" onClick={reset}>
+          Reset
+        </button>
       </div>
     </div>
   );
